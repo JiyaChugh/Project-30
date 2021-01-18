8a8384c403227595551da24938815247885b3be6
JiyaChugh <74240256+JiyaChugh@users.noreply.github.com>
2021-01-18 22:49:25 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,26 @@
+class Box {
+    constructor(x, y, width, height) {
+        var options = {
+            friction: 0,
+            restitution: 0.4
+        }
+        this.body = Bodies.rectangle(x,y,width,height,options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+    }
+    display() {
+        if(this.body.speed<3){
+        var pos =this.body.position;
+        rectMode(CENTER);
+        rect(pos.x, pos.y, this.width, this.height);
+        }
+        else{
+            World.remove(world, this.body);
+            push();
+            this.visibility = this.visibility-5;
+            TransitionEvent(255,this.visibility);
+            pop();
+        }
+    }
+};
